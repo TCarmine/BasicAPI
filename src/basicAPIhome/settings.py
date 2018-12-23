6a7f5dff37345919437dc3eac4f005de66e82fef
Carmine <kappa85t@hotmail.it>
2018-12-23 23:26:14 +0100

    added apps in settings and Framework variable in setting+ Pygments

--- src/basicAPIhome/settings.py
+++ src/basicAPIhome/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #installed
+    # 'rest_framework',
+    'posting',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
